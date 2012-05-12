d2cea22fab479d9e34daa9c08b17be96b9bfb93a
Åke Forslund <ake.forslund@gmail.com>
2012-05-13 00:27:29 +0200

    Minor cleanup, moved each parallax level's state and info to an object

--- lib/parallax.py
+++ lib/parallax.py
@@ -4,14 +4,18 @@ from pygame.locals import *
 
 SCREENRECT = Rect(0,0, 640, 480)
 
+class parallaxSubSurface:
+	def __init__(self, s, f):
+		self.scroll = 0
+		self.factor = f
+		self.surface = s
+
 class parallaxSurface:
 	def __init__(self):
 		print "parllaxSurface inited!"
 		self.numLevels = 0
-		self.parallaxSurfaces = []
-		self.sc = []
-		self.factor = []
 		self.scroller = 0
+		self.parallaxLevels = []
 	def addLevel(self, imagePath, scrollFactor):
 		try:
 			image = (pygame.image.load(imagePath))
@@ -21,20 +25,18 @@ class parallaxSurface:
 
 		image.set_colorkey((0xff,0x00,0xea))
 		image = image.convert()
-		self.parallaxSurfaces.append(image)
-		self.factor.append(scrollFactor)
-		self.sc.append(0)
+		self.parallaxLevels.append(parallaxSubSurface(image, scrollFactor))
 		self.numLevels += 1
 
 	def draw(self, surface):
 		i = 0
-		for s in self.parallaxSurfaces:
-			surface.blit(s, (0,0), (self.sc[i], 0, SCREENRECT.width, SCREENRECT.height))
-			surface.blit(s, (s.get_width() - self.sc[i], 0), (0, 0, self.sc[i], SCREENRECT.height))
+		for l in self.parallaxLevels:
+			surface.blit(l.surface, (0,0), (l.scroll, 0, SCREENRECT.width, SCREENRECT.height))
+			surface.blit(l.surface, (l.surface.get_width() - l.scroll, 0), (0, 0, l.scroll, SCREENRECT.height))
 			i += 1
 
 	def scroll(self, offset):
 		self.scroller = (self.scroller + offset)
-		for i in range(len(self.sc)):
-			self.sc[i] = (self.scroller / self.factor[i]) % self.parallaxSurfaces[i].get_width()
+		for l in self.parallaxLevels:
+			l.scroll = (self.scroller / l.factor) % l.surface.get_width()
 
