ff3444ec29ba56ac00cc15a90d5a2fb63a909e06
Åke Forslund <ake.forslund@gmail.com>
2021-02-09 21:07:40 +0100

    Make this a pip-installable module

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='pyparallax',
+    version='0.1',
+    packages=['parallax'],
+    install_requires=['pygame'],
+    url='https://github.com/forslund/pyParallax',
+    license='Apache-2.0',
+    author='Åke Forslund',
+    author_email='ake.forslund@gmail.com',
+    description='A simple parallax effect using pygame',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
