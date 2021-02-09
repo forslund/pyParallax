5e43529cf83182069c31820ac515c4ea93743286
Åke Forslund <ake.forslund@gmail.com>
2021-02-09 21:11:58 +0100

    Update demo code to be more fault tolerant

--- demo/demo.py
+++ demo/demo.py
@@ -16,25 +16,24 @@
 #
 
 
-import os, sys
+from os.path import dirname, join
+
 import pygame
 from pygame.locals import *
 
-sys.path.append ("../")
 import parallax
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480), pygame.DOUBLEBUF)
-pygame.display.set_caption('Parallax-test')
+pygame.display.set_caption('Parallax-demo')
 pygame.mouse.set_visible(0)
 
-orientation = 'vertical'
-
-
 bg = parallax.ParallaxSurface((640, 480), pygame.RLEACCEL)
-bg.add('p2.png', 5)
-bg.add('p3.png', 2)
-bg.add('p1.png', 1)
+
+directory = dirname(__file__)
+bg.add(join(directory, 'p2.png'), 5)
+bg.add(join(directory, 'p3.png'), 2)
+bg.add(join(directory, 'p1.png'), 1)
 
 run = True
 speed = 0
@@ -44,21 +43,16 @@ while run:
         if event.type == QUIT:
             run = False
         if event.type == KEYDOWN and event.key == K_RIGHT:
-            speed += 10
+            speed += 2
         if event.type == KEYUP and event.key == K_RIGHT:
-            speed -= 10
+            speed -= 2
         if event.type == KEYDOWN and event.key == K_LEFT:
-            speed -= 10
+            speed -= 4
         if event.type == KEYUP and event.key == K_LEFT:
-            speed += 10
-        if event.type == KEYDOWN and event.key == K_UP:
-            orientation = 'vertical'
-        if event.type == KEYDOWN and event.key == K_DOWN:
-            orientation = 'horizontal'
-        
-    bg.scroll(speed, orientation)
+            speed += 4
+
+    bg.scroll(speed)  # Move the background with the set speed
     t = pygame.time.get_ticks()
     if (t - t_ref) > 60:
         bg.draw(screen)
         pygame.display.flip()
-
